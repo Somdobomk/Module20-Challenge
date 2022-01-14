234a99e15931e39e0a3c3fbc6934aa7dff3c592d
somdobomk <somdobomk@gmail.com>
2022-01-14 17:56:33 -0600

    setup project folder structure and finished card.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Card from './components/Card';
+
+function App() {
+  return (
+    <div className='min-h-screen py-10 px-3 sm:px-5 bg-gray-100'>
+      <Card />
+    </div>
+  );
+}
+
+export default App;
