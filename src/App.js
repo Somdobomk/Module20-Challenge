b8d1db17b6a18bbe68586266e281c951dad559e3
somdobomk <somdobomk@gmail.com>
2022-01-20 20:55:37 -0600

    added group project 3 to projects section

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom'
+import Navbar from './components/Navbar';
 import Card from './components/Card';
 import About from './components/About';
 import Footer from './components/Footer';
@@ -18,6 +20,14 @@ function App() {
   return (
     <div className='min-h-screen py-10 px-3 sm:px-5 bg-gray-100'>
       <div data-aos='fade-down' data-aos-duration='800'>
+      <Navbar />
+      </div>
+      <div
+      data-aos='fade-zoom-in'
+      data-aos-easing='ease-in-back'
+      data-aos-delay='300'
+      data-aos-duration='800'
+      >
         <Card />
       </div>
       <div data-aos='fade-up' data-aos-duration='800' data-aos-delay='400'>
@@ -28,6 +38,26 @@ function App() {
       <ContactMe />
       <Footer />
     </div>
+    //   <Routes>
+    //     <Route path='/' element={<Card />} />
+    //     <Route path='/about' element={<About />} />
+    //     <Route path='/skills' element={<Skills />} />
+    //     <Route path='/projects' element={<Projects />} />
+    //     <Route path='/contact' element={<ContactMe />} />
+
+    // <div className='min-h-screen py-10 px-3 sm:px-5 bg-gray-100'>
+    //   <div data-aos='fade-down' data-aos-duration='800'>
+    //     <Card />
+    //   </div>
+    //   <div data-aos='fade-up' data-aos-duration='800' data-aos-delay='400'>
+    //     <About />
+    //   </div>
+    //   <Skills />
+    //   <Projects />
+    //   <ContactMe />
+    //   <Footer />
+    // </div>
+    // </Routes>
   );
 }
 
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,11 +4,12 @@ import project2 from '../images/project2.jpg';
 import project3 from '../images/project3.jpg';
 import project4 from '../images/project4.jpg';
 import project5 from '../images/project5.jpg';
+import project6 from '../images/project6.gif';
 
 function Projects() {
   return (
-      <div className='max-w-4xl mx-auto justify-center py-12' id='projects'>
-        <p className='text-2xl text-black text-center sm:text-4xl pt-4 font-bold'>
+      <div className='justify-center max-w-4xl py-12 mx-auto' id='projects'>
+        <p className='pt-4 text-2xl font-bold text-center text-black sm:text-4xl'>
           Projects
         </p>
         <div className='flex flex-wrap justify-center pt-2'>
@@ -16,11 +17,11 @@ function Projects() {
         <div className=''>
           <img className='w-32 mx-auto shadow-xl drop-shadow-sm' src={project1} alt='project1' />
         </div>
-        <div className='text-center mt-5'>
-          <p className='text-xl sm:text-2xl text-gray-900 font-semibold'>
+        <div className='mt-5 text-center'>
+          <p className='text-xl font-semibold text-gray-900 sm:text-2xl'>
             <a href={'https://ourdoggos.com/'} target='_blank' rel='noopener noreferrer'>Ourdoggos</a>
           </p>
-          <p className='text-xs sm:text-base text-gray-600 pt-2 pb-4 px-5 w-auto inline-block'>
+          <p className='inline-block w-auto px-5 pt-2 pb-4 text-xs text-gray-600 sm:text-base'>
           <a href={'/'} target='_blank' rel='noopener noreferrer'>Source</a>
           </p>
         </div>
@@ -30,11 +31,11 @@ function Projects() {
         <div className=''>
           <img className='w-32 mx-auto shadow-xl drop-shadow-sm' src={project2} alt='project2' />
         </div>
-        <div className='text-center mt-5'>
-          <p className='text-xl sm:text-2xl text-gray-900 font-semibold'>
+        <div className='mt-5 text-center'>
+          <p className='text-xl font-semibold text-gray-900 sm:text-2xl'>
           <a href={'https://ddtcoffee.com'} target='_blank' rel='noopener noreferrer'>DDT Coffee</a>
           </p>
-          <p className='text-xs sm:text-base text-gray-600 pt-2 pb-4 px-5 w-auto inline-block'>
+          <p className='inline-block w-auto px-5 pt-2 pb-4 text-xs text-gray-600 sm:text-base'>
           <a href={'/'} target='_blank' rel='noopener noreferrer'>Source</a>
           </p>
         </div>
@@ -44,11 +45,11 @@ function Projects() {
         <div className=''>
           <img className='w-32 mx-auto shadow-xl drop-shadow-sm' src={project3} alt='project3' />
         </div>
-        <div className='text-center mt-5'>
-          <p className='text-xl sm:text-2xl text-gray-900 font-semibold'>
+        <div className='mt-5 text-center'>
+          <p className='text-xl font-semibold text-gray-900 sm:text-2xl'>
           <a href={'https://safetycomplianceandtraining.com'} target='_blank' rel='noopener noreferrer'>SCT</a>
           </p>
-          <p className='text-xs sm:text-base text-gray-600 pt-2 pb-4 px-5 w-auto inline-block'>
+          <p className='inline-block w-auto px-5 pt-2 pb-4 text-xs text-gray-600 sm:text-base'>
           <a href={'/'} target='_blank' rel='noopener noreferrer'>Source</a>
           </p>
         </div>
@@ -58,11 +59,11 @@ function Projects() {
         <div className=''>
           <img className='w-32 mx-auto shadow-xl drop-shadow-sm' src={project4} alt='project4' />
         </div>
-        <div className='text-center mt-5'>
-          <p className='text-xl sm:text-2xl text-gray-900 font-semibold'>
+        <div className='mt-5 text-center'>
+          <p className='text-xl font-semibold text-gray-900 sm:text-2xl'>
           <a href={'https://dogecoin-ticker.netlify.app'} target='_blank' rel='noopener noreferrer'>Dogecoin Ticker</a>
           </p>
-          <p className='text-xs sm:text-base text-gray-600 pt-2 pb-4 px-5 w-auto inline-block'>
+          <p className='inline-block w-auto px-5 pt-2 pb-4 text-xs text-gray-600 sm:text-base'>
           <a href={'https://github.com/Somdobomk/Dogecoin-Ticker'} target='_blank' rel='noopener noreferrer'>Source</a>
           </p>
         </div>
@@ -72,12 +73,26 @@ function Projects() {
         <div className=''>
           <img className='w-32 mx-auto shadow-xl drop-shadow-sm' src={project5} alt='project5' />
         </div>
-        <div className='text-center mt-5'>
-          <p className='text-xl sm:text-2xl text-gray-900 font-semibold'>
+        <div className='mt-5 text-center'>
+          <p className='text-xl font-semibold text-gray-900 sm:text-2xl'>
           <a href={'https://github.com/Somdobomk/Dogecoin-Ticker'} target='_blank' rel='noopener noreferrer'>Portfolio V1</a>
           </p>
-          <p className='text-xs sm:text-base text-gray-600 pt-2 pb-4 px-5 w-auto inline-block'>
+          <p className='inline-block w-auto px-5 pt-2 pb-4 text-xs text-gray-600 sm:text-base'>
           <a href={'/'} target='_blank' rel='noopener noreferrer'>Source</a>
+          </p>
+        </div>
+      </div>
+
+      <div className='flex flex-col w-40 p-10 m-6 overflow-hidden bg-white shadow-2xl rounded-xl sm:w-52'>
+        <div className=''>
+          <img className='w-32 mx-auto shadow-xl drop-shadow-sm' src={project6} alt='project5' />
+        </div>
+        <div className='mt-5 text-center'>
+          <p className='text-xl font-semibold text-gray-900 sm:text-2xl'>
+          <a href={'https://somdobomk-module20-challenge.netlify.app/'} target='_blank' rel='noopener noreferrer'>ECommerce Store</a>
+          </p>
+          <p className='inline-block w-auto px-5 pt-2 pb-4 text-xs text-gray-600 sm:text-base'>
+          <a href={'https://github.com/Somdobomk/burkholder-eyelashes'} target='_blank' rel='noopener noreferrer'>Source</a>
           </p>
         </div>
       </div>
